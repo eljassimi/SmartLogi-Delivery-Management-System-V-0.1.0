087a34e123684747a458528487f66c17fa2c550f
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-27 21:03:28 +0100

    Creaye Livreur Service

--- src/main/java/com/smartlogi/controller/LivreurController.java
+++ src/main/java/com/smartlogi/controller/LivreurController.java
@@ -1,4 +1,27 @@
 package com.smartlogi.controller;
 
+
+import com.smartlogi.model.Livreur;
+import com.smartlogi.service.LivreurService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/livreur")
 public class LivreurController {
+
+    private final LivreurService livreurService;
+
+    public LivreurController(LivreurService livreurService){
+        this.livreurService = livreurService;
+    }
+
+    @GetMapping
+    public List<Livreur> getAllLivreur(){
+        return livreurService.findAll();
+    }
+
 }
