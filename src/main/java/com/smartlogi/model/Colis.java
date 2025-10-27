5d3deaf8d3edfa816031736d8effbc7ba8d0cb97
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-27 11:49:27 +0100

    create colis model

---
+++ src/main/java/com/smartlogi/model/Colis.java
@@ -0,0 +1,18 @@
+package com.smartlogi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Colis {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Double poids;
+    private String statut;
+    private String priorite;
+    private String villeDestination;
+}
