1e280d584c53f6824ba678522f380b14d859cd69
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-27 14:16:01 +0100

    create client expediteur repository

---
+++ src/main/java/com/smartlogi/repository/ClientExpediteurRepository.java
@@ -0,0 +1,7 @@
+package com.smartlogi.repository;
+
+import com.smartlogi.model.ClientExpediteur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientExpediteurRepository extends JpaRepository<ClientExpediteur,Long> {
+}
