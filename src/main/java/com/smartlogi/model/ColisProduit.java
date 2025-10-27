9900896ac49ec048b5c2ba2c2c8823da50e9da02
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-27 16:23:13 +0100

    create a class for the many to many relation between colis and product

--- src/main/java/com/smartlogi/model/ColisProduit.java
+++ src/main/java/com/smartlogi/model/ColisProduit.java
@@ -1,4 +1,28 @@
 package com.smartlogi.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
 public class ColisProduit {
+
+    @EmbeddedId
+    private ColisProduitId id;
+
+    @ManyToOne
+    @MapsId("colisId")
+    @JoinColumn(name = "id_colis")
+    private Colis colis;
+
+    @ManyToOne
+    @MapsId("produitId")
+    @JoinColumn(name = "id_produit")
+    private Produit produit;
+
+    private Integer quantite;
+    private Double prix;
+    private LocalDate dateAjout;
 }
