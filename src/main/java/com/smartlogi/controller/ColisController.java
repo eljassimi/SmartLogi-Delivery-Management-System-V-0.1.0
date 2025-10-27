fae85224da5a818e371d14886fe7abd861d23dd7
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-27 12:15:47 +0100

    src/main/java/com/smartlogi/controller/ColisController.java

--- src/main/java/com/smartlogi/controller/ColisController.java
+++ src/main/java/com/smartlogi/controller/ColisController.java
@@ -1,4 +1,28 @@
 package com.smartlogi.controller;
 
+import com.smartlogi.model.Colis;
+import com.smartlogi.repository.ColisRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/colis")
 public class ColisController {
+
+    private final ColisRepository colisRepository;
+
+    public ColisController(ColisRepository colisRepository) {
+        this.colisRepository = colisRepository;
+    }
+
+    @GetMapping
+    public List<Colis> getAllColis() {
+        return colisRepository.findAll();
+    }
+
+    @PostMapping
+    public Colis createColis(@RequestBody Colis colis) {
+        return colisRepository.save(colis);
+    }
 }
