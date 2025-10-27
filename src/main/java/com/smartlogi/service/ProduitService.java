90a15ccdd951ac32378a9f54ac44d5c109614f50
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-27 15:52:21 +0100

    create produit service

--- src/main/java/com/smartlogi/service/ProduitService.java
+++ src/main/java/com/smartlogi/service/ProduitService.java
@@ -1,4 +1,34 @@
 package com.smartlogi.service;
 
+import com.smartlogi.model.Produit;
+import com.smartlogi.repository.ProduitRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProduitService {
+
+    private final ProduitRepository produitRepository;
+
+    public ProduitService(ProduitRepository produitRepository) {
+        this.produitRepository = produitRepository;
+    }
+
+    public List<Produit> findAll() {
+        return produitRepository.findAll();
+    }
+
+    public Optional<Produit> findById(Long id) {
+        return produitRepository.findById(id);
+    }
+
+    public Produit save(Produit produit) {
+        return produitRepository.save(produit);
+    }
+
+    public void delete(Long id) {
+        produitRepository.deleteById(id);
+    }
 }
