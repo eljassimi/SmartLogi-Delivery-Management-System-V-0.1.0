7b4dcc0b74d1238868682d92b546b16dda0f0000
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-27 15:52:03 +0100

    create produit repository

--- src/main/java/com/smartlogi/repository/ProduitRepository.java
+++ src/main/java/com/smartlogi/repository/ProduitRepository.java
@@ -1,4 +1,7 @@
 package com.smartlogi.repository;
 
-public interface ProduitRepository {
+import com.smartlogi.model.Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProduitRepository extends JpaRepository<Produit,Long> {
 }
