9a1c73e21c4b40109a3edbd49ef1cf21fc7b7b0c
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-27 15:33:12 +0100

    create destinataire controller

--- src/main/java/com/smartlogi/controller/DestinataireController.java
+++ src/main/java/com/smartlogi/controller/DestinataireController.java
@@ -1,4 +1,39 @@
 package com.smartlogi.controller;
 
+import com.smartlogi.model.Destinataire;
+import com.smartlogi.service.DestinataireService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/destinataires")
 public class DestinataireController {
+
+    private final DestinataireService destinataireService;
+
+    public DestinataireController(DestinataireService destinataireService) {
+        this.destinataireService = destinataireService;
+    }
+
+    @GetMapping
+    public List<Destinataire> getAll() {
+        return destinataireService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Destinataire> getById(@PathVariable Long id) {
+        return destinataireService.findById(id);
+    }
+
+    @PostMapping
+    public Destinataire create(@RequestBody Destinataire destinataire) {
+        return destinataireService.save(destinataire);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        destinataireService.delete(id);
+    }
 }
