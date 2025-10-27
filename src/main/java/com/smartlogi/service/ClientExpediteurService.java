d43ec6cf2702b60a4d46cb410739737b50fe060d
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-27 21:01:17 +0100

    Create Client Expediteur Service

--- src/main/java/com/smartlogi/service/ClientExpediteurService.java
+++ src/main/java/com/smartlogi/service/ClientExpediteurService.java
@@ -1,4 +1,34 @@
 package com.smartlogi.service;
 
+import com.smartlogi.model.ClientExpediteur;
+import com.smartlogi.repository.ClientExpediteurRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClientExpediteurService {
+
+    private final ClientExpediteurRepository clientExpediteurRepository;
+
+    public ClientExpediteurService(ClientExpediteurRepository clientExpediteurRepository){
+        this.clientExpediteurRepository = clientExpediteurRepository;
+    }
+
+    public List<ClientExpediteur> findAll(){
+        return clientExpediteurRepository.findAll();
+    }
+
+    public Optional<ClientExpediteur> findById(Long id){
+        return clientExpediteurRepository.findById(id);
+    }
+
+    public ClientExpediteur save(ClientExpediteur clientExpediteur){
+        return clientExpediteurRepository.save(clientExpediteur);
+    }
+
+    public void delete(Long id){
+        clientExpediteurRepository.deleteById(id);
+    }
 }
