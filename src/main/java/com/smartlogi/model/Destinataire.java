c73bca39af7bf4f36c09b52e068c18fe2c875303
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-27 14:26:06 +0100

    create Desitinataire Model

---
+++ src/main/java/com/smartlogi/model/Destinataire.java
@@ -0,0 +1,17 @@
+package com.smartlogi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Destinataire {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private String adresse;
+}
