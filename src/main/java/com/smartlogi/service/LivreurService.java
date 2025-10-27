d2c5f3198efdce9646310341262c75408dd8ff2a
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-27 21:01:50 +0100

    Create Livreur Service

--- src/main/java/com/smartlogi/service/LivreurService.java
+++ src/main/java/com/smartlogi/service/LivreurService.java
@@ -1,4 +1,35 @@
 package com.smartlogi.service;
 
+import com.smartlogi.model.Livreur;
+import com.smartlogi.repository.LivreurRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class LivreurService {
+
+    private final LivreurRepository livreurRepository;
+
+    public LivreurService(LivreurRepository livreurRepository){
+        this.livreurRepository = livreurRepository;
+    }
+
+    public List<Livreur> findAll(){
+        return  livreurRepository.findAll();
+    }
+
+    public Optional<Livreur> findById(Long id){
+        return livreurRepository.findById(id);
+    }
+
+    public Livreur save(Livreur livreur){
+        return livreurRepository.save(livreur);
+    }
+
+    public void delete(Long id){
+        livreurRepository.deleteById(id);
+    }
+
 }
