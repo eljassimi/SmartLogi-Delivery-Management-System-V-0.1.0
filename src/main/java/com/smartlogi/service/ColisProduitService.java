521f0e7524cd49a51a24fad11530e174755cbc55
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-27 16:56:18 +0100

    Create Colis Produit Service

--- src/main/java/com/smartlogi/service/ColisProduitService.java
+++ src/main/java/com/smartlogi/service/ColisProduitService.java
@@ -1,4 +1,35 @@
 package com.smartlogi.service;
 
+import com.smartlogi.model.ColisProduit;
+import com.smartlogi.model.ColisProduitId;
+import com.smartlogi.repository.ColisProduitRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ColisProduitService {
+
+    private final ColisProduitRepository colisProduitRepository;
+
+    public ColisProduitService(ColisProduitRepository colisProduitRepository) {
+        this.colisProduitRepository = colisProduitRepository;
+    }
+
+    public List<ColisProduit> findAll() {
+        return colisProduitRepository.findAll();
+    }
+
+    public Optional<ColisProduit> findById(ColisProduitId id) {
+        return colisProduitRepository.findById(id);
+    }
+
+    public ColisProduit save(ColisProduit colisProduit) {
+        return colisProduitRepository.save(colisProduit);
+    }
+
+    public void delete(ColisProduitId id) {
+        colisProduitRepository.deleteById(id);
+    }
 }
