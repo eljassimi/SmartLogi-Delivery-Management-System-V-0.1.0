71fd750ad07463e1263bede54330a8a911c93714
eljassimi-maccasy <abdel.eljassii@maccasy.com>
2025-11-02 12:01:26 +0100

    Add Livreur request DTO

---
+++ src/main/java/com/smartlogi/dto/livreur/LivreurRequestDTO.java
@@ -0,0 +1,19 @@
+package com.smartlogi.dto.livreur;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LivreurRequestDTO {
+    @NotNull @Size(min = 2, max = 100)
+    private String nom;
+    @NotNull @Size(min = 2, max = 100)
+    private String prenom;
+    @NotNull @Size(min = 8, max = 20)
+    private String telephone;
+    @NotNull @Size(min = 3, max = 50)
+    private String vehicule;
+    @NotNull @Size(min = 2, max = 100)
+    private String zoneAssignee;
+}
