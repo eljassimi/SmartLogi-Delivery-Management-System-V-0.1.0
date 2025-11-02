8693d7fe3c02da92d624f3bb1a3040d03ae305f5
eljassimi-maccasy <abdel.eljassii@maccasy.com>
2025-11-02 12:01:42 +0100

    Add Livreur response DTO

---
+++ src/main/java/com/smartlogi/dto/livreur/LivreurResponseDTO.java
@@ -0,0 +1,13 @@
+package com.smartlogi.dto.livreur;
+
+import lombok.Data;
+
+@Data
+public class LivreurResponseDTO {
+    private String id;
+    private String nom;
+    private String prenom;
+    private String telephone;
+    private String vehicule;
+    private String zoneAssignee;
+}
