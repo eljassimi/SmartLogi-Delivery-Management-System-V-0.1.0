c49491d8806ad6de3208edcb4c77c163616959ae
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-11-06 21:20:18 +0100

    add zone repository

---
+++ src/main/java/com/smartlogi/repository/ZoneRepository.java
@@ -0,0 +1,9 @@
+package com.smartlogi.repository;
+
+import com.smartlogi.model.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, String> {
+}
+
+
